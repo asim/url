1584c554b1ca6c088d5bac4504279e4a3325db60
Asim Aslam <asim@hailocab.com>
2014-09-30 08:26:25 +0100

    First

---
+++ asl.am.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bytes"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+var (
+	dir    = "/home/asim/cache"
+	domain = "asl.am"
+	words  []string
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+
+	b, err := ioutil.ReadFile("/usr/share/dict/words")
+	if err != nil {
+		panic("cant get words")
+	}
+
+	for _, word := range bytes.Split(b, []byte("\n")) {
+		words = append(words, strings.ToLower(string(word)))
+	}
+}
+
+func encode(u string) string {
+	return base64.StdEncoding.EncodeToString([]byte(u))
+}
+
+func decode(u string) string {
+	d, _ := base64.StdEncoding.DecodeString(u)
+	return string(d)
+}
+
+func random() string {
+	var word string
+	i := len(words) - 1
+
+	for len(word) < 512 {
+		word += (words[rand.Int()%i] + "-")
+	}
+
+	return word[:len(word)-1]
+}
+
+func main() {
+	http.HandleFunc("/u/", func(w http.ResponseWriter, r *http.Request) {
+		parts := strings.Split(r.URL.Path, "/")
+		if len(parts) < 3 {
+			http.Redirect(w, r, "/", 302)
+			return
+		}
+
+		u := decode(parts[2])
+		pu, err := url.Parse(u)
+		if err != nil || !pu.IsAbs() {
+			http.Redirect(w, r, "/", 302)
+			return
+		}
+
+		http.Redirect(w, r, u, 301)
+	})
+
+	http.HandleFunc("/lengthen", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+
+		u := r.Form.Get("url")
+		pu, err := url.Parse(u)
+		if err != nil || !pu.IsAbs() {
+			http.Error(w, "Invalid url", 502)
+			return
+		}
+
+		ul := fmt.Sprintf("http://%s/u/%s/%s", domain, encode(u), random())
+		fmt.Fprintf(w, ul)
+
+	})
+
+	log.Fatal(http.ListenAndServe(":9999", nil))
+}
